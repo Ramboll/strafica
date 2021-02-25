725b8ad06046dfb91ea814db7dc6ffd1f0e47864
mlhollestelle <martijn.hollestelle@ramboll.fi>
2021-02-25 16:39:17 +0200

    fix error and implement suggestion

--- R/geospatial.R
+++ R/geospatial.R
@@ -1,23 +1,19 @@
 #' Read points, lines or polygons from a geospatial file
 #'
-#' @param fname name of geospatial file
+#' @param fname name of geospatial file (.shp, .gpkg or .TAB)
 #' @return A \code{Spatial*DataFrame} object.
 #' @export read_geospatial_file
 read_geospatial_file = function(fname) {
-    file_ext = get_file_ext(new_connection)
-    if(file_ext %nin% c(".shp", ".gpkg", ".TAB")) {
-        error_message = messagef("Filetype '%s' not supported.
-                                 Please use '.shp', .gpkg' or '.TAB'")
+    file_ext = get_file_ext(fname)
+    if(file_ext == ".shp") {
+        return(read.shape(fname))
+    } else if(file_ext == ".gpkg") {
+        return(read.gpkg(fname))
+    } else if(file_ext == ".TAB") {
+        return(read.mapinfo(fname))
+    } else {
+        error_message = messagef("Filetype '%s' not supported. Please use '.shp', .gpkg' or '.TAB'",
+                                 file_ext)
         stop(error_message)
     }
-    if(file_ext == ".shp") {
-        geospatial_file = read.shape(new_connection)
-    }
-    if(file_ext == ".gpkg") {
-        geospatial_file = read.gpkg(new_connection)
-    }
-    if(file_ext == ".TAB") {
-        geospatial_file = read.mapinfo(new_connection)
-    }
-    return(geospatial_file)
 }
