01f4236ec18b9a04ff1f25efe94ccf4eb1bf3a84
Rainer Kujala <rainer.kujala@ramboll.fi>
2021-10-18 11:55:35 +0300

    Remove shapefiles, retain geojson for polygons

--- tests/testthat/test_verbosity_settings.R
+++ tests/testthat/test_verbosity_settings.R
@@ -14,16 +14,34 @@ remove_all_shape_file_parts = function(main_shapefile) {
     }
 }
 
-test_that("with verbosity unset, there will be messages when writing out a shapefile", {
+get_dummy_lines = function() {
+    # Creating lines by creating first 
+    # segments (a dataframe with custom columns)
+    seg = data.frame(eid = 1, sid = 1,
+                     ix = 1, iy = 1,
+                     jx = 2, jy = 2)
+    data = data.frame(eid = 1, value = 1337)
+    lines = list(lines = segments.to.lines(seg), data = data)
+    return(lines)
+}
+
+get_dummy_lines_as_sp = function() {
+    dummy_lines = get_dummy_lines()
+    # Convert lines to a SpatialLinesDataFrame
+    lines_as_spdf = lines.to.sp(dummy_lines$lines,
+                                data=dummy_lines$data,
+                                verbose=FALSE)
+    return(lines_as_spdf)
+}
+
+
+test_that("with verbose option being unset, there will be messages when writing out a shapefile", {
     dummy_epsg_code = 3067
-    spatial_data_frame_lines = strafica::read.shape(
-        "../data/one_line.shp",
-        verbose = FALSE
-    )
+    dummy_lines_spdf = get_dummy_lines_as_sp()
     dummy_file_name_shp = tempfile(fileext = ".shp")
     expect_message(
         strafica::write.shape(
-            spatial_data_frame_lines,
+            dummy_lines_spdf,
             dummy_file_name_shp,
             epsg = dummy_epsg_code
         ),
@@ -34,14 +52,11 @@ test_that("with verbosity unset, there will be messages when writing out a shape
 
 test_that("with verbose=FALSE, there will be no messages when writing out a shapefile", {
     dummy_epsg_code = 3067
-    spatial_data_frame_lines = strafica::read.shape(
-        "../data/one_line.shp",
-        verbose = FALSE
-    )
+    dummy_lines_spdf = get_dummy_lines_as_sp()
     dummy_file_name_shp = tempfile(fileext = ".shp")
     expect_message(
         strafica::write.shape(
-            spatial_data_frame_lines,
+            dummy_lines_spdf,
             dummy_file_name_shp,
             epsg = dummy_epsg_code,
             verbose = FALSE
@@ -53,37 +68,30 @@ test_that("with verbose=FALSE, there will be no messages when writing out a shap
 
 
 test_that("verbose=FALSE option can suppress messages with sp.to.lines", {
-    spatial_data_frame_lines = strafica::read.shape(
-        "../data/one_line.shp",
-        verbose = FALSE,
-    )
+    dummy_lines_spdf = get_dummy_lines_as_sp()
     expect_message(
-        strafica::sp.to.lines(spatial_data_frame_lines),
+        strafica::sp.to.lines(dummy_lines_spdf),
         NULL
     )
     expect_message(
-        strafica::sp.to.lines(spatial_data_frame_lines, verbose = FALSE),
+        strafica::sp.to.lines(dummy_lines_spdf, verbose = FALSE),
         NA
     )
 })
 
 test_that("verbose=FALSE option can suppress messages with lines.to.sp", {
-    spatial_data_frame_lines = strafica::read.shape(
-        "../data/one_line.shp",
-        verbose = FALSE
-    )
-    lines_as_two_data_frames = strafica::sp.to.lines(spatial_data_frame_lines)
+    dummy_lines = get_dummy_lines()
     expect_message(
         strafica::lines.to.sp(
-            lines_as_two_data_frames$lines,
-            lines_as_two_data_frames$data
+            dummy_lines$lines,
+            dummy_lines$data
         ),
         NULL
     )
     expect_message(
         strafica::lines.to.sp(
-            lines_as_two_data_frames$lines,
-            lines_as_two_data_frames$data,
+            dummy_lines$lines,
+            dummy_lines$data,
             verbose = FALSE
         ),
         NA
@@ -91,8 +99,8 @@ test_that("verbose=FALSE option can suppress messages with lines.to.sp", {
 })
 
 test_that("verbose=FALSE option can suppress output with sp.to.polys", {
-    spatial_data_frame_polys = strafica::read.gpkg(
-        "../data/two_polygons.gpkg",
+    spatial_data_frame_polys = strafica::read.geojson(
+        "test_data_two_polygons.geojson",
         verbose = FALSE,
     )
     expect_message(
@@ -106,8 +114,8 @@ test_that("verbose=FALSE option can suppress output with sp.to.polys", {
 })
 
 test_that("verbose=FALSE option can suppress output with polys.to.sp", {
-    spatial_data_frame_polys = strafica::read.gpkg(
-        "../data/two_polygons.gpkg",
+    spatial_data_frame_polys = strafica::read.geojson(
+        "test_data_two_polygons.geojson",
         verbose = FALSE,
     )
     spatial_polygons_as_list = strafica::sp.to.polys(spatial_data_frame_polys)
